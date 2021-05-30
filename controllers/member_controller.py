403f3f986c1d4efc676f2e36a3962bfbcc1aca3e
jandrewgordon <jandrewgordon@gmail.com>
2021-05-30 12:05:37 +0100

    input and premium checkbox for new members working

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -8,3 +8,27 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repository.select_all()
     return render_template("members/index.html", all_members = members)
+
+@members_blueprint.route("/members/new", methods=['GET'])
+def get_new_member():
+    return render_template("members/new.html")
+
+
+@members_blueprint.route("/members", methods=['POST'])
+def create_new_member():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    premium = request.form.get("premium")
+    if premium:
+        print("We laughing")
+        premium = True
+    else:
+        print("gubbed")
+        premium = False
+    bookings = 0
+    new_member = Member(last_name, first_name, premium, bookings)
+    member_repository.save(new_member)
+    return redirect("/members")
+
+
+    
